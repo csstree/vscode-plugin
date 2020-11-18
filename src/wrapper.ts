085ca07e59ae9876606803b8805d0908745239f4
Roman Dvornov <rdvornov@gmail.com>
2020-11-19 00:32:47 +0100

    Fixes

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -6,18 +6,22 @@ let { validate } = require('csstree-validator/lib/validate');
 export function wrapper({ code }) {
   let errors = validate(code);
   let diagnostics: Diagnostic[] = [];
-console.log({ errors });
+
   errors.forEach(({ line, column, loc, message }) => {
     let severity: DiagnosticSeverity = DiagnosticSeverity.Warning;
     let range: Range;
 
     if (loc && loc.start && loc.end) {
-      range.start = { line: line - 1, character: column - 1 };
-      range.end = { line: loc.end.line - 1, character: loc.end.column - 1 };
+      range = {
+        start: { line: line - 1, character: column - 1 },
+        end: { line: loc.end.line - 1, character: loc.end.column - 1 }
+      };
     } else {
       severity = DiagnosticSeverity.Error;
-      range.start = { line: line - 1, character: column - 1 };
-      range.end = { line: line - 1, character: column };
+      range = {
+        start: { line: line - 1, character: column - 1 },
+        end: { line: line - 1, character: column }
+      };
     }
 
     diagnostics.push({
